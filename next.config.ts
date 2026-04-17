16078057c9ed0e4c86c22900e809d2a2fb8fb47f
Maski <maxlucasaleixo@gmail.com>
2026-04-17 17:24:58 -0300

    fit: configuring the archive  next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  basePath: '/mavic', 
+  output: 'export', // Ativa a exportação estática
+  // Remova a linha abaixo se o site for o seu domínio principal (usuario.github.io)
+  basePath: '/nome-do-repositorio', 
   images: {
-    unoptimized: true, 
+    unoptimized: true, // Necessário pois o Pages não otimiza imagens automaticamente
   },
 };
 
